4b4fc49e89f1bd428e0a9014541ed1ca7b2ae00a
saramaher <sara.almaher94@gmail.com>
2021-04-16 12:49:09 +0200

    create PutMapping

--- src/main/java/se/kth/sda/skeleton/ResourceNotFoundException.java
+++ src/main/java/se/kth/sda/skeleton/ResourceNotFoundException.java
@@ -1,2 +1,7 @@
-package se.kth.sda.skeleton;public class ResourceNotFoundException {
+package se.kth.sda.skeleton;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException  extends RuntimeException {
 }
--- src/main/java/se/kth/sda/skeleton/posts/PostRepository.java
+++ src/main/java/se/kth/sda/skeleton/posts/PostRepository.java
@@ -1,2 +1,6 @@
-package se.kth.sda.skeleton.posts;public interface PostRepository {
+package se.kth.sda.skeleton.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post,Long> {
 }
