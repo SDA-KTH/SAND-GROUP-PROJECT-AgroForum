570cd31079ac226e18610d615612a7b483fa5c10
Ayatj <79480683+Ayatj@users.noreply.github.com>
2021-04-17 11:31:25 +0200

    Update poste class

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,10 +1,11 @@
 // NPM Packages
 import React from "react";
+// Home page
 export default function HomePage() {
   return (
     <div className="card">
       <div className="card-body">
-        <h4 className="card-title">SDA starter template</h4>
+        <h4 className="card-title">SAND starter template</h4>
         <p>
           This starter template is based on Spring, PostgreSQL, React, React
           router and Axios. Check the following links for documentation and
