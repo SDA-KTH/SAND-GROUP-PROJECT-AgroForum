f3a5977c515e3e9b54abde34a18a960218a4f54e
Dakouri  Kobri <maurille.kobri@gmail.com>
2021-04-19 08:45:26 +0200

    Add UserController exihibiting GET method

---
+++ src/main/java/se/kth/sda/skeleton/user/UserController.java
@@ -0,0 +1,30 @@
+package se.kth.sda.skeleton.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RequestMapping("/users")
+@RestController
+public class UserController {
+
+    UserRepository userRepository;
+    UserService userService;
+
+    @Autowired
+    public UserController(UserRepository userRepository, UserService userService) {
+        this.userRepository = userRepository;
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<User> getUser(Principal principal){
+        String userName = principal.getName();
+        User user = userService.findUserByEmail(userName);
+        return ResponseEntity.ok(user);
+    }
+}
